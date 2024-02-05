dcd79f358f7b53bc818cf56c7e191e49e49c683e
Evan Dadson <edadson@ncsu.edu>
2024-02-05 13:27:25 -0500

    update to tmp_and_prcp script

--- tmp_and_prcp.R
+++ tmp_and_prcp.R
@@ -1,31 +1,44 @@
 library(terra)
-# library(lubridate)
 
-# Temperature germination severity function
-tsv <- function(tmean) {
-  tsv_values <- ifelse(tmean <= 12, 0,
-                            ifelse(tmean > 12 & tmean < 32, 100*(-0.07+0.88*(exp(-0.5*(log(tmean/23.08)/0.28)^2))),
-                                        ifelse(tmean >= 32, 0,0)))
-  return(tsv_values)
+
+
+# Path to file output location
+outpath <- "/Volumes/cmjone25/Data/Raster/USA/pops_casestudies/citrus_black_spot/"
+start_year <- 2011
+end_year <- 2011
+days_per_year <- 30
+
+
+
+# Temperature germination function
+temp_fun <- function(tmean) {
+  temp_values <- ifelse(tmean > 12 & tmean < 32, 100*(-0.07+0.88*(exp(-0.5*(log(tmean/23.08)/0.28)^2))), 0)
+  return(temp_values)
 }
 
-# Precipitation severity indicator function
-psv <- function(prcp) {
+
+
+# Precipitation indicator function
+prcp_fun <- function(prcp) {
   prcp_values <- ifelse(prcp < 0.25, 0, 1)
   return(prcp_values)
 }
 
-# Florida shapefile
+
+
+# Florida boundary
 florida <- vect("/Volumes/cmjone25/Data/Raster/USA/pops_casestudies/citrus_black_spot/cb_2018_us_state_20m/cb_2018_us_state_20m.shp")
 florida <- florida[florida$NAME == "Florida"]
 
-# Path to file output location
-outpath <- "/Volumes/cmjone25/Data/Raster/USA/pops_casestudies/citrus_black_spot/"
 
-# Create temp and prcp rasters for each year and day
-for (year in 2011:2017) {
-  for (day in 1:365) {
-    prcp <- rast(paste0("/Volumes/cmjone25/Data/Original/Daymet/precip/daymet_v3_prcp_", year, "_na.nc4"))[[day]]
+
+# Create temp and prcip raster files for each year and day
+for (year in seq(start_year, end_year)) {
+  
+  for (day in 1:days_per_year) {
+    
+    # Read rasters
+    prcp <- rast(paste0("/Volumes/cmjone25/Data/Original/Daymet/precip/daymet_v3_prcp_", 2011, "_na.nc4"))[[18]]
     tmax <- rast(paste0("/Volumes/cmjone25/Data/Original/Daymet/tmin/daymet_v3_tmin_", year, "_na.nc4"))[[day]]
     tmin <- rast(paste0("/Volumes/cmjone25/Data/Original/Daymet/tmax/daymet_v3_tmax_", year, "_na.nc4"))[[day]]
     
@@ -36,7 +49,7 @@ for (year in 2011:2017) {
     prcp <- terra::crop(prcp, florida)
     
     # Apply precipitation indicator function to raster
-    psv_values <- app(prcp, fun = psv)
+    prcp_values <- app(prcp, fun = prcp_fun)
     
     # Crop temp rasters to florida extent
     tmin <- terra::crop(tmin, florida)
@@ -46,9 +59,14 @@ for (year in 2011:2017) {
     tmean <- (tmin + tmax) / 2
     
     # Apply temperature severity function to raster
-    tsv_values <- app(tmean, fun = tsv)
+    temp_values <- app(tmean, fun = temp_fun)
     
-    writeRaster(psv_values, paste0(test_out, "temp_coeffs_", year, "/", "temp_coeff_", year, "_", day, "_.tif"), overwrite = TRUE)
-    writeRaster(tsv_values, paste0(test_out, "prcp_coeffs_", year, "/", "prcp_coeff_", year, "_", day, "_.tif"), overwrite = TRUE)
+    # Write rasters
+    writeRaster(prcp_values, paste0(outpath, "precip/","prcp_coeff_", year, "_", day, "_.tif"), overwrite = TRUE)
+    writeRaster(temp_values, paste0(outtpath, "temp/","temp_coeff_", year, "_", day, "_.tif"), overwrite = TRUE)
+    
   }
+  
 }
+
+
